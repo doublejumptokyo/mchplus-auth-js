01fee0c57c5f3c34a67c91b30fd077703b1c1489
makzent <arnold.michael91@gmail.com>
2020-04-13 19:37:14 +0900

    added auth and cleaned up code

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,33 @@
-const DOMAIN__SAND = 'https://auth.mch.plus.sand.pls.djty.co/'
-const DOMAIN__PROD = 'https://auth.mch.plus/'
 
-const env = {
-  sand: DOMAIN__SAND,
-  prod: DOMAIN__PROD
+interface Env {
+  sand: {
+    clientId: string
+    lang: string
+    authUri: string
+    verifyNumberUri: string
+  },
+  prod: {
+    clientId: string
+    lang: string
+    authUri: string
+    verifyNumberUri: string
+  },
+
 }
 
+
+const env: Env = {
+  sand: {
+    clientId: "pls-sand",
+    lang: "ja",
+    authUri: "https://auth.mch.plus.sand.pls.djty.co/authorize",
+    verifyNumberUri: "https://auth.mch.plus.sand.pls.djty.co/verify-number"
+  },
+  prod: {
+    clientId: "pls-prod",
+    lang: "ja",
+    authUri: "https://auth.mch.plus/authorize",
+    verifyNumberUri: "https://auth.mch.plus/verify-number"
+  }
+}
 export default env
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,36 @@
-import env from './env'
 import VerifyNumber from './verify-number'
 
 export class MchplusAuth {
-  private clientId: string
   private web3: any
   private env: string
   private verifyNumber: VerifyNumber
 
 
-  constructor(clientId, web3, number, env = 'sand', ) {
-    this.clientId = clientId
+  constructor(web3, env = 'sand', lang = 'en') {
     this.env = env
     this.web3 = web3
-    this.verifyNumber = new VerifyNumber(clientId, web3, number, env)
+    this.verifyNumber = new VerifyNumber(web3, env, lang)
 
     this.checkInput()
   }
 
-  get baseUrl(): string {
-    return this.env === 'prod' ? env.prod : env.sand
-  }
-
   checkInput() {
-    console.info('mchplus auth initialized')
     console.info('-------------------------')
+    console.info('[mchplus auth] initialized')
     console.info('env :', this.env)
-    console.info('clientId :', this.clientId)
     console.info('web3 :', this.web3)
-    console.info('number :', this.number)
+    console.info('-------------------------')
+    if (this.env !== ('sand' || 'prod')) {
+      throw Error('Incorrect env specified, please use either sand or prod')
+    }
   }
 
   async getNumberRegions() {
     return await this.verifyNumber.getRegions()
   }
 
-  get number() {
-    return this.verifyNumber.checkNumber
+  async submitNumber(phoneNumber, isCall) {
+    return await this.verifyNumber.submitInput(phoneNumber, isCall)
   }
 }
 

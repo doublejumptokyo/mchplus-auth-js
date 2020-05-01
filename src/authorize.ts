0cd075dcc2258ae5b3feb22ef5bf4b8f4c97c2fd
Michael Arnold <arnold.michael91@gmail.com>
2020-05-01 11:35:02 +0900

    changed auth path back

--- src/authorize.ts
+++ src/authorize.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import Cookies from 'universal-cookie';
 import env from './env'
+const cookies = new Cookies();
 
 export class Authorize {
   private clientId: string
@@ -7,18 +9,26 @@ export class Authorize {
   private env: string
   private lang: string
   private message: string
+  private cookieName: string
 
   defaultAccount: string
 
-  constructor(clientId, web3, env, lang = 'en') {
+  constructor(clientId, web3, env, lang = 'en', cookieName) {
     this.clientId = clientId
     this.web3 = web3
     this.env = env
     this.lang = lang
+    this.cookieName = cookieName
   }
 
   get baseUrl(): string {
     return this.env === 'prod' ? env.prod.authUri : env.sand.authUri
+  }
+
+  get state(): string {	
+    const state = Math.floor(Math.random() * 100000);	
+    cookies.set(this.cookieName, state, { maxAge: 600 });	
+    return String(state);	
   }
 
   async getAddress() {
@@ -27,10 +37,16 @@ export class Authorize {
   }
 
   async getMessage() {
+    const address = await this.getAddress()
     const params = new URLSearchParams();
+    params.append("response_type", "code");	
+    params.append("scope", "openid profile");
     params.append("client_id", this.clientId);
+    params.append("state", this.state);	
+    params.append("redirect_uri", "http://localhost:3000/callback");	
+    params.append("address", address);
     params.append("lang", this.lang);
-    const url = "/api/client?" + params.toString();
+    const url = "/api/authorize?" + params.toString();
     const ret = await axios.get(`${this.baseUrl}${url}`);
     return ret.data
   }
@@ -54,7 +70,9 @@ export class Authorize {
         client_id: this.clientId,
         signature: signature,
         network: "mainnet", // todo: allow other networks
+        redirect_uri: "http://localhost:3000/callback", // todo: make optional on server
         lang: this.lang,
+        state: this.state
       });
       return res
     } catch (err) {

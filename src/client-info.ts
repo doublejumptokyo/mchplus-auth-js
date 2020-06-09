e0edb597dc601eb907ccde15d247bdff17541744
shunfuruya <shun.furuya1987@gmail.com>
2020-06-09 13:33:01 +0900

    delete getRegions getAddress

--- src/client-info.ts
+++ src/client-info.ts
@@ -3,26 +3,19 @@ import env from './env'
 
 export class ClientInfo {
   private clientId: string
-  private web3: any
   private env: string
   private lang: string
 
   defaultAccount: string
 
-  constructor(clientId, web3, env, lang = 'en') {
+  constructor(clientId, env, lang = 'en') {
     this.clientId = clientId
-    this.web3 = web3
     this.env = env
     this.lang = lang
   }
 
   get baseUrl(): string {
     return this.env === 'prod' ? env.prod.authUri : env.sand.authUri
-  }
-
-  async getAddress() {
-    const accounts = await this.web3.eth.getAccounts()
-    return accounts.pop()
   }
 
   async getClientInfo() {

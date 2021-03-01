0640ed98adc4522b3f0e3d97ef68687500b83feb
Michael Arnold <arnold.michael91@gmail.com>
2021-03-01 16:27:19 +0900

    refactored to ethers.js

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,71 @@
-import VerifyNumber from './verify-number'
-import Authorize from './authorize'
-import ClientInfo from './client-info'
-import LoginQrcode from './login-qrcode'
-import Utils from './utils'
+import VerifyNumber from "./verify-number";
+import Authorize from "./authorize";
+import ClientInfo from "./client-info";
+import LoginQrcode from "./login-qrcode";
+import Utils from "./utils";
+import { ethers, providers } from "ethers";
 
 export class MchplusAuth {
-  private web3: any
-  private env: string
-  private verifyNumber: VerifyNumber
-  private authorize: Authorize
-  private clientInfo: ClientInfo
-  private loginQrcode: LoginQrcode
-  private utils: Utils
+  private web3: providers.Web3Provider;
+  private env: string;
+  private verifyNumber: VerifyNumber;
+  private authorize: Authorize;
+  private clientInfo: ClientInfo;
+  private loginQrcode: LoginQrcode;
+  private utils: Utils;
 
+  constructor(clientId = "localhost", provider, env = "sand", lang = "en") {
+    this.web3 = new ethers.providers.Web3Provider(provider, "any");
+    this.env = env;
+    this.verifyNumber = new VerifyNumber(clientId, provider, env, lang);
+    this.authorize = new Authorize(clientId, provider, env, lang);
+    this.clientInfo = new ClientInfo(clientId, env, lang);
+    this.loginQrcode = new LoginQrcode(clientId, env);
+    this.utils = new Utils(provider);
 
-  constructor(clientId = "localhost", web3, env = 'sand', lang = 'en') {
-    this.web3 = web3
-    this.env = env
-    this.verifyNumber = new VerifyNumber(clientId, web3, env, lang)
-    this.authorize = new Authorize(clientId, web3, env, lang)
-    this.clientInfo = new ClientInfo(clientId, env, lang)
-    this.loginQrcode = new LoginQrcode(clientId, env)
-    this.utils = new Utils(web3)
-
-    this.checkInput()
+    this.checkInput();
   }
 
   checkInput() {
-    console.info('%c[mchplus auth] initialized','background: #222; color: #bada55')
-    if (!(this.env === 'sand' || this.env === 'prod')) {
-      throw Error('Incorrect env specified, please use either sand or prod')
+    console.info(
+      "%c[mchplus auth] initialized",
+      "background: #222; color: #bada55"
+    );
+    if (!(this.env === "sand" || this.env === "prod")) {
+      throw Error("Incorrect env specified, please use either sand or prod");
     }
     if (!this.web3) {
-      throw Error('No Web3 instance detected.')
+      throw Error("No Web3 instance detected.");
     }
   }
 
   async getNumberRegions() {
-    return await this.verifyNumber.getRegions()
+    return await this.verifyNumber.getRegions();
   }
 
   async submitNumber(phoneNumber, isCall) {
-    return await this.verifyNumber.submitInput(phoneNumber, isCall)
+    return await this.verifyNumber.submitInput(phoneNumber, isCall);
   }
 
   async confirmNumber(confirmationPin) {
-    return await this.verifyNumber.submitConfirm(confirmationPin)
+    return await this.verifyNumber.submitConfirm(confirmationPin);
   }
 
   async signAuth(callbackUrl) {
-    return await this.authorize.sign(callbackUrl)
-  }
-  
-  async getClientInfo(){
-    return await this.clientInfo.getClientInfo()
+    return await this.authorize.sign(callbackUrl);
   }
 
-  async getQRCode(callbackUrl, state){
-    return await this.loginQrcode.getQRCode(callbackUrl, state)
+  async getClientInfo() {
+    return await this.clientInfo.getClientInfo();
   }
 
-  get getWalletName():string {
-    return this.utils.walletName
+  async getQRCode(callbackUrl, state) {
+    return await this.loginQrcode.getQRCode(callbackUrl, state);
+  }
+
+  get getWalletName(): string {
+    return this.utils.walletName;
   }
 }
 
-export default MchplusAuth
+export default MchplusAuth;
--- src/utils.ts
+++ src/utils.ts
@@ -1,103 +1,102 @@
-declare var navigator:any;
+declare var navigator: any;
 export class Utils {
-  private web3
+  private provider;
 
-  constructor(web3) {
-    this.web3 = web3
+  constructor(provider) {
+    this.provider = provider;
   }
 
   get walletName(): string {
-    const potentialWallet = this.findWalletName(this.web3.currentProvider)
+    const potentialWallet = this.findWalletName(this.provider);
 
-    if (!this.web3) return 'noWeb3';
-
-    if (!this.web3.currentProvider) return 'noProviderInfo'
+    if (!this.provider) return "noProvider";
 
     // https://brave.com/
-    if (navigator.brave) return 'brave';
+    if (navigator.brave) return "brave";
 
     // https://metamask.io/
-    if (this.web3.currentProvider.isMetaMask) return 'metamask';
+    if (this.provider.isMetaMask) return "metamask";
 
     // https://tor.us/
-    if (this.web3.currentProvider.isTorus) return 'torus';
+    if (this.provider.isTorus) return "torus";
 
     // https://www.opera.com/ja/crypto
-    if (this.web3.currentProvider.isOpera) return 'opera';
+    if (this.provider.isOpera) return "opera";
 
     // https://tokenpocket.jp/ja/
-    if (this.web3.currentProvider.isTokenPocket) return 'tokenPocket';
+    if (this.provider.isTokenPocket) return "tokenPocket";
 
     // https://trustwallet.com/
-    if (this.web3.currentProvider.isTrust) return 'trust';
+    if (this.provider.isTrust) return "trust";
 
     // https://www.go-wallet.app/
-    if (this.web3.currentProvider.isGoWallet) return 'goWallet';
+    if (this.provider.isGoWallet) return "goWallet";
 
     // https://status.im/
-    if (this.web3.currentProvider.isStatus) return 'status';
+    if (this.provider.isStatus) return "status";
 
     // https://wallet.coinbase.com/
-    if (this.web3.currentProvider.isToshi) return 'coinbase';
+    if (this.provider.isToshi) return "coinbase";
 
     // https://www.meetdapper.com/
-    if (this.web3.currentProvider.isDapper) return 'dapper';
+    if (this.provider.isDapper) return "dapper";
 
     // https://developer.samsung.com/blockchain/platform/programming-guide/cucumber-webview.html
-    if (this.web3.currentProvider.isCucumber) return 'samsung';
+    if (this.provider.isCucumber) return "samsung";
 
     // https://tokenpocket.jp/ja/
-    if (this.web3.currentProvider.isTokenPocketCrypton) return 'dedicatedTokenPocketApp';
+    if (this.provider.isTokenPocketCrypton) return "dedicatedTokenPocketApp";
 
     // https://alphawallet.com/
-    if (this.web3.currentProvider.isAlpha) return 'alphaWallet';
+    if (this.provider.isAlpha) return "alphaWallet";
 
     // https://qurage.app/en
-    if (this.web3.currentProvider.isQurage) return 'qurage';
+    if (this.provider.isQurage) return "qurage";
 
     // https://goldenwallet.io/
-    if (this.web3.currentProvider.isGolden) return 'golden';
+    if (this.provider.isGolden) return "golden";
 
     // https://techcrunch.com/2018/04/13/coinbase-acquires-decentralized-app-browser-wallet-cipher-browser/
-    if (this.web3.currentProvider.isCipher) return 'cipher';
+    if (this.provider.isCipher) return "cipher";
 
     // https://www.myetherwallet.com/
-    if (this.web3.currentProvider.isMew) return 'mew';
+    if (this.provider.isMew) return "mew";
 
     // https://token.im/
-    if (this.web3.currentProvider.isImToken) return 'imToken';
+    if (this.provider.isImToken) return "imToken";
 
     // https://www.dapppocket.io/
-    if (this.web3.currentProvider.isDappPocket) return 'dappPocket';
+    if (this.provider.isDappPocket) return "dappPocket";
 
     // https://www.alicedapp.com/
-    if (this.web3.currentProvider.isAlice) return 'alice';
+    if (this.provider.isAlice) return "alice";
 
     // https://alphawallet.com/
-    if (this.web3.currentProvider.isAlphaWallet) return 'alphaWallet';
+    if (this.provider.isAlphaWallet) return "alphaWallet";
 
     // https://www.coinomi.com/en/
-    if (this.web3.currentProvider.isCoinomi) return 'coinomi';
+    if (this.provider.isCoinomi) return "coinomi";
 
     // https://mykey.org/
-    if (this.web3.currentProvider.isMYKEY) return 'mykey';
+    if (this.provider.isMYKEY) return "mykey";
 
     // https://qpocket.io/
-    if (this.web3.currentProvider.isQPocket) return 'qPocket';
+    if (this.provider.isQPocket) return "qPocket";
 
     return `unknown: ${potentialWallet}`;
   }
 
   findWalletName(currentProvider) {
-    let potentialWalletName = ''
+    let potentialWalletName = "";
     for (let key in currentProvider) {
-       if (key.startsWith('is')) {
-            potentialWalletName = !potentialWalletName.length ? key : potentialWalletName.concat(', ', key)
-       }
+      if (key.startsWith("is")) {
+        potentialWalletName = !potentialWalletName.length
+          ? key
+          : potentialWalletName.concat(", ", key);
+      }
     }
-    return potentialWalletName
+    return potentialWalletName;
   }
 }
 
-
-export default Utils
+export default Utils;
